9da4f6d3aea3d4dd91333d0f3f2c9aa46a0eb706
daidtech <dvdai63@gmail.com>
2024-11-09 15:17:41 +0700

    restructure folder

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "../components/ui/button"
+
+export default function Home() {
+  return (
+    <div>
+      {/* primary color */}
+      <Button className="bg-primary text-white">HOme page</Button>
+    </div>
+  )
+}
