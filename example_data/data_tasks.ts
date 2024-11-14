b1d9a979e149e6b57d508742c242cae1570aa169
daidtech <dvdai63@gmail.com>
2024-11-14 07:16:31 +0700

    add id and status fields to example tasks

--- example_data/data_tasks.ts
+++ example_data/data_tasks.ts
@@ -6,18 +6,24 @@ const exampleTasks: Task[] = [
     description: 'Description for Task 1',
     priority: 'High',
     assignee: 'John Doe',
+    id: "",
+    status: "in-progress"
   },
   {
     title: 'Task 2',
     description: 'Description for Task 2',
     priority: 'Medium',
     assignee: 'Jane Smith',
+    id: "",
+    status: "in-progress"
   },
   {
     title: 'Task 3',
     description: 'Description for Task 3',
     priority: 'Low',
     assignee: 'Alice Johnson',
+    id: "",
+    status: "in-progress"
   },
 ];
 
